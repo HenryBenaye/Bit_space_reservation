c59dabd624105cd6490fa7e80e410880695c5d39
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-10-31 22:31:38 +0100

    updates

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreReservationRequest;
 use App\Models\Reservation;
 use App\Models\Space;
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -38,18 +40,12 @@ class ReservationController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param StoreReservationRequest $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(StoreReservationRequest $request)
     {
-        $request->validate([
-            'space_name' => 'required|exists:spaces,name',
-            'begin_time_hour' => 'required|integer',
-            'begin_time_minute' => 'required|integer',
-            'end_time_hour' => 'required|integer',
-            'end_time_minute' => 'required|integer',
-        ]);
+
 
         $user = User::find(Auth::user()->id);
         $space = Space::where('name', $request['space_name'])->first();
@@ -122,26 +118,7 @@ class ReservationController extends Controller
 
     private function data_check($user,$space, $begin_time, $end_time, $type )
     {
-        $time_check = Reservation::where('user_id',  $user->id)
-            ->where('begin_time' , '>=', $begin_time->format('H:i'))
-            ->where('end_time', '<=', $end_time->format('H:i'))->get();
 
-        // Controles uitvoeren of de gebruiker juiste data heeft ingevoerd
-        if (!$end_time->isAfter($begin_time) || $begin_time->floatDiffInMinutes($end_time) > 60)
-        {
-            return redirect()->back()->withErrors(['msg' => 'Je moet minimaal 15 minuten reserveren of je hebt meer dan een uur gereserveerd']);
-        }
-        if (!$time_check->isEmpty())
-        {
-            return redirect()->back()->withErrors(['msg' => 'Je hebt al een reservering tussen die tijd']);
-        }
-        if($user->reservations === 5) {
-            return redirect()->back()->withErrors(['msg' => 'Je hebt al 5 reserveringen']);
-        }
-        if($space->max_students - $space->reserved_students <= 0)
-        {
-            return redirect()->back()->withErrors(['msg' => 'Ruimte is vol']);
-        }
 
         $user->reservations++;
         $space->reserved_students++;
--- app/Http/Requests/StoreReservationRequest.php
+++ app/Http/Requests/StoreReservationRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Reservation;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreReservationRequest extends FormRequest
@@ -30,5 +31,29 @@ class StoreReservationRequest extends FormRequest
             'end_time_hour' => 'required|integer',
             'end_time_minute' => 'required|integer',
         ];
+    }
+
+    public function time_validation()
+    {
+        $time_check = Reservation::where('user_id',  $user->id)
+            ->where('begin_time' , '>=', $begin_time->format('H:i'))
+            ->where('end_time', '<=', $end_time->format('H:i'))->get();
+
+        // Controles uitvoeren of de gebruiker juiste data heeft ingevoerd
+        if (!$end_time->isAfter($begin_time) || $begin_time->floatDiffInMinutes($end_time) > 60)
+        {
+            return redirect()->back()->withErrors(['msg' => 'Je moet minimaal 15 minuten reserveren of je hebt meer dan een uur gereserveerd']);
+        }
+        if (!$time_check->isEmpty())
+        {
+            return redirect()->back()->withErrors(['msg' => 'Je hebt al een reservering tussen die tijd']);
+        }
+        if($user->reservations === 5) {
+            return redirect()->back()->withErrors(['msg' => 'Je hebt al 5 reserveringen']);
+        }
+        if($space->max_students - $space->reserved_students <= 0)
+        {
+            return redirect()->back()->withErrors(['msg' => 'Ruimte is vol']);
+        }
     }
 }
