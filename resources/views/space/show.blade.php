842161b51112cf1fe0d99fab8f267125bbf43a1a
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-11-29 14:59:35 +0100

    Update show.blade.php

--- resources/views/space/show.blade.php
+++ resources/views/space/show.blade.php
@@ -3,18 +3,21 @@
         <h1 class="font-bold text-2xl">{{$SpaceName}}</h1>
         <span class=" text-xl">Max students: {{$MaxStudents}}</span>
     </div>
-    <div class="grid grid-rows-4 grid-flow-col gap-4" >
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" >
         @foreach($reservations as $reservation)
-            <div class="p-6 bg-white border-b border-gray-200">
-                <p class="font-bold">Name</p>
-                <p>{{$reservation->user->name}}</p>
-                <p class="font-bold">Time</p>
-                <p>{{$date =\Carbon\Carbon::parse($reservation->begin_time)->format('d-m-Y')}}</p>
-                <p>{{date("H:i", strtotime($reservation->begin_time))}}-{{date("H:i", strtotime($reservation->end_time))}}</p>
+            <div class="bg-white overflow-hidden mb-4 shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <p class="font-bold">Name</p>
+                    <p>{{$reservation->user->name}}</p>
+                    <p class="font-bold">Time</p>
+                    <p>{{$date =\Carbon\Carbon::parse($reservation->begin_time)->format('d-m-Y')}}</p>
+                    <p>{{date("H:i", strtotime($reservation->begin_time))}}-{{date("H:i", strtotime($reservation->end_time))}}</p>
+                </div>
             </div>
         @endforeach
+            <div class="mt-4">
+                {{$reservations->links()}}
+            </div>
     </div>
-    <div class="mt-4">
-        {{$reservations->links()}}
-    </div>
+
 </x-app-layout>
